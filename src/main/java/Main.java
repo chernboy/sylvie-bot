f3fcadcd3f63117b09bc1da0ea58b6266eb030ce
chernboy <michael_chern@hotmail.com>
2019-01-30 16:50:10 -0800

    removed token + added handler class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,14 +1,17 @@
+import io.magicthegathering.javasdk.resource.Card;
 import net.dv8tion.jda.core.AccountType;
 import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.MessageBuilder;
+import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
-
 import javax.security.auth.login.LoginException;
 
 public class Main extends ListenerAdapter {
     public static void main(String[] args) throws LoginException {
         JDABuilder builder = new JDABuilder(AccountType.BOT);
-        String token = "NTMzMTA5MzQzMTE0MjMxODA5.DxmQ3A.PnLpU2Q4ovTN19tHCgYyoQIzu8E";
+
+        String token = Token.token;
         builder.setToken(token);
         builder.addEventListener(new Main());
         builder.buildAsync();
@@ -22,19 +25,36 @@ public class Main extends ListenerAdapter {
         }
 
         // logs messages received in console
+        // TODO: Remove later
         System.out.println("received message from " +
                 event.getAuthor().getName() + ": " +
                 event.getMessage().getContentDisplay()
         );
 
         // responds to messages
-        //TODO: change block to switch statement
-        if (event.getMessage().getContentRaw().equals("&hello")) {
-            event.getChannel().sendMessage("World").queue();
-        } else if (event.getMessage().getContentRaw().equals("u w u")) {
-            event.getChannel().sendMessage("n w n").queue();
-        } else if (event.getMessage().getContentRaw().equals("n w n")) {
-            event.getChannel().sendMessage("u w u").queue();
+        String msge =  event.getMessage().getContentRaw();
+        String cmd = getCmd(msge);
+
+        switch (cmd) {
+            case "&hello":
+                event.getChannel().sendMessage("World").queue();
+                break;
+            case "u w u":
+                event.getChannel().sendMessage("n w n").queue();
+                break;
+            case "n w n":
+                event.getChannel().sendMessage("u w u").queue();
+                break;
+            case "&mtg":
+                //TODO:
+                break;
         }
+    }
+
+    private String getCmd(String str) {
+        if (str.startsWith("&")) {
+            return str.split("\\s")[0];
+        }
+        return str;
     }
 }
